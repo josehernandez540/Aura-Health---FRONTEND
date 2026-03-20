031f25289b0a30f6be2c0cf63ecb670f76d90d7b
Jose Daniel Hernandez Cano <j.hernandez@cyclopslab.com>
2026-03-19 20:29:17 -0500

    feat(frontend): configuración cliente HTTP con axios e interceptores

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    console.error(error.response?.data);
+    return Promise.reject(error);
+  }
+);
